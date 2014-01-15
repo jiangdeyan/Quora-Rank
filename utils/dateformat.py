6a49863fde0f7672e0d713978e2e2c9786826567
jiangdeyan@gmail.com <jiangdeyan@jiangdeyan-VirtualBox.(none)>
2014-01-15 18:07:00 +0800

    ADD

--- utils/dateformat.py
+++ utils/dateformat.py
@@ -1,4 +1,5 @@
 from quoratz import quoratz
+from datetime import datetime, timedelta
 
 UPDATED = 'Updated'
 UPDATED_LEN = 7
@@ -18,23 +19,22 @@ MONTH = {
 		'Sep' : 9,
 		'Oct' : 10,
 		'Nov' : 11,
-		'Dec' : 12,
+		'Dec' : 12
 		}
 
-WeekDay = {
-		'Mon' : 0
-		'Tue' : 1
-		'Wed' : 2
-		'Thu' : 3
-		'Fri' : 4
-		'Sat' : 5
+WEEKDAY = {
+		'Mon' : 0,
+		'Tue' : 1,
+		'Wed' : 2,
+		'Thu' : 3,
+		'Fri' : 4,
+		'Sat' : 5,
 		'Sun' : 6
 		}
 
 def getTimeString(datestring):
-
 	# like 'Just now'
-	if datestring.find(JUST_NOW):
+	if datestring.find(JUST_NOW) > -1:
 		return datetime.now(tz=quoratz()).date().isoformat()
 	cut = datestring
 	index_update = cut.find(UPDATED)
@@ -48,61 +48,58 @@ def getTimeString(datestring):
 	for temp in AM_PM:
 		index_ap = cut.find(temp)
 		if index_ap > -1:
-			return handle_am_pm(cut,ap,index_ap)
+			return handle_am_pm(cut, temp, index_ap)
 	#like "xxx ago"
 	if cut.find(AGO) > 0:
 		return handle_ago(cut)
 	#only week 
 	if len(cut) == 3:
 		return handle_week(cut) 
-	return handle
+	return handle_dmy(cut)
 
 def handle_am_pm(date, ap, index):
-	hour = -1
 	hour_s = date[:index]
-	try:
-		hour = int(hour_s)
-		if ap == AM_PM[1] and ap <= 11:
-			hour += 12
-		now = datetime.now(tz=quoratz())
-		then = now.replace(hour=hour)
-		return then.date().isoformat()
-	except:
-		print 'should log here'
+	hour_i = str2int(hour_s)
+	if hour_i == -1:
+		print "log here"
 		return None
+	elif ap == AM_PM[1] and hour_i <= 11:
+		hour_i += 12
+	now = datetime.now(tz=quoratz())
+	then = now.replace(hour=hour_i)
+	return then.date().isoformat()
 
 
 def handle_ago(date):
+	# 'xxh ago'
 	index_h = date.find('h')
 	if index_h > -1:
 		hour_s = date[:index_h]
-		try:
-			hour = int(hour_s)
-			assert hour > 0
+		hour_i = str2int(hour_s)
+		if hour_i > -1:
 			now = datetime.now(tz=quoratz())
-			then = now + timedelta(hour=-hour)
-			return then.date().isoformat() 
-		except:
-			print "should log here"
+			then = now + timedelta(hours=-hour_i)
+			return then.date().isoformat()
+		else:
+			print 'log here'
 			return None
+	# 'xxm ago'
 	index_m = date.find('m')
 	if index_m > -1:
 		minu_s = date[:index_m]
-		try:
-			minu = int(minu_s)
+		minu = str2int(minu_s)
+		if minu > -1:
 			now = datetime.now(tz=quoratz())
-			then = now + timedelta(minute=-minu)
+			then = now + timedelta(minutes=-minu)
 			return then.date().isoformat()
-		except:
+		else:
 			print "should log here"
 			return None
 
 def handle_week(date):
-	week = -1
-	try:
-		week = WeekDay[date]
-	except:
-		print 'should log here'
+	week = getDict(date, WEEKDAY)
+	if week == -1:
+		print 'log error'
 		return None
 	now = datetime.now(tz=quoratz())
 	now_week = now.weekday()
@@ -122,32 +119,25 @@ def handle_dmy(date):
 	index_space = date.find(' ')
 	if index_space > -1:
 		day_s = date[:index_space]
-		try:
-			day_i = int(day_s)
-		except:
-			print 'log here'
-			return None
+		day_i = str2int(day_s)
 		index_comma = date.find(',')
 		if index_comma > -1:
 			month_s = date[index_space+1 : index_comma]
-			try:
-				mouth_i = MONTH[month_s]
-			except:
-				print 'log here'
-				return None
-			year_s = date[index_comma:]
-			try:
-				year_i = int(year_s)
-				if day_i > -1 and month_i > -1 and year_i > -1
+			month_i = getDict(month_s, MONTH)	
+			year_s = date[index_comma+1:]
+			year_i = str2int(year_s)
+			if day_i > -1 and month_i > -1 and year_i > -1:
 				now = datetime.now(tz=quoratz())
-				then = now.replace(day = day_i, month = month_i)
-			except:
+				then = now.replace(year = year_i, day = day_i, month = month_i)
+				return then.date().isoformat()
+			else:
 				print 'log here'
 				return None
-			
 		else:
-			now = datetime.now(tz=quoratz())
+			month_s = date[index_space+1 :]
+			month_i = getDict(month_s, MONTH)
 			if day_i > -1 and month_i > -1:
+				now = datetime.now(tz=quoratz())
 				then = now.replace(day = day_i, month = month_i)
 				return then.date().isoformat()
 			else:
@@ -157,8 +147,33 @@ def handle_dmy(date):
 		print 'should log here'
 		return None
 
+def str2int(s):
+	try:
+		i = int(s)
+		return i
+	except:
+		print 'cast error log here'
+		return -1
 
+def getDict(s, d):
+	try:
+		ret = d[s]
+		return ret
+	except:
+		print 'dic key error'
+		return -1
+def test(s):
+	print s, "|  ",getTimeString(s)
 
+if __name__ == '__main__':
+	#test("Updated 2am")
+	#test("Mon")
+	#test("Updated Tue")
+	#test("11h ago")
+	#test("6 Jan")
+	#test("Updated 24 Dec")
+	test("3 Dec, 2012")
+	test("Updated 10 Dec, 2012")
 
 
 
